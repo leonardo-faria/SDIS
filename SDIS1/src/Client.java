266802fd87a7e243e8fd14a35bdcddf6d22ddd2a
leonardo-faria <leonardofaria94@gmail.com>
2015-03-04 11:19:51 +0000

    Signed-off-by: leonardo-faria <leonardofaria94@gmail.com>

--- SDIS1/src/Client.java
+++ SDIS1/src/Client.java
@@ -5,15 +5,26 @@ public class Client {
 	public static void main(String[] args) throws IOException {
 		DatagramSocket socket = new DatagramSocket();
 		byte[] buf;
-		if (args[0].equals("LOOKUP"))
-			buf = (args[0] + " " + args[1]).getBytes();
-		else
-			buf = (args[0] + " " + args[1] + " " + args[2]).getBytes();
-		InetAddress address = InetAddress.getLocalHost();
+		if (args[2].equals("LOOKUP"))
+			buf = (args[2] + " " + args[3]).getBytes();
+		else if (args[0].equals("REGISTER"))
+			buf = (args[2] + " " + args[3] + " " + args[4]).getBytes();
+		else {
+			System.out.println("not valid");
+			socket.close();
+			return;
+		}
+		InetAddress address = InetAddress.getByName(args[0]);
 		DatagramPacket packet = new DatagramPacket(buf, buf.length, address,
-				8000);
+				Integer.parseInt(args[1]));
 		socket.send(packet);
-		socket.close();System.out.println(new String(packet.getData()));
+		System.out.println(new String(packet.getData()));
+		buf = new byte[512];
+		DatagramPacket recieved = new DatagramPacket(buf, buf.length);
+		socket.receive(recieved);
+		System.out.println(new String(recieved.getData()));
+		
+		socket.close();
 	}
 
 }
--- SDIS1/src/Server.java
+++ SDIS1/src/Server.java
@@ -2,35 +2,41 @@ import java.awt.print.Printable;
 import java.io.IOException;
 import java.net.*;
 import java.util.ArrayList;
+import java.util.Hashtable;
 
 public class Server {
 	public static void main(String[] args) throws IOException {
-		ArrayList<Plate> plates = new ArrayList<Plate>();
+		Hashtable<String, String> plates = new Hashtable<String, String>();
+		@SuppressWarnings("resource")
 		DatagramSocket socket = new DatagramSocket(Integer.parseInt(args[0]));
-		byte[] buf = new byte[256];
 
+		String answer = new String();
 		while (true) {
-			DatagramPacket packet = new DatagramPacket(buf, buf.length);
-			socket.receive(packet);
-			System.out.println(new String(packet.getData()));
-			System.out.println();
-			String[] r = new String(packet.getData()).split("\\s");
-			if (r[0].equals("LOOKUP") && r.length == 2) {
-				try {
-					Plate.lookup(plates, r[1]);
-				} catch (Exception e) {
-					System.out.println("FAILE");
-				}
-			} else if (r[0].equals("REGISTER") && r.length == 3) {
-				try {
-					Plate.lookup(plates, r[1]);
-					System.out.println("FAIL");
-				} catch (Exception e) {
-					plates.add(new Plate(r[2], r[1]));
-				}
+			byte[] buf = new byte[512];
+			DatagramPacket recieved = new DatagramPacket(buf, buf.length);
+			socket.receive(recieved);
+			System.out.println(new String(recieved.getData()));
+			String[] r = new String(recieved.getData()).split("\\s");
+			for (int i = 0; i < r.length; i++) {
+				r[i] = r[i].trim();
 			}
-			socket.receive(packet);
-			System.out.println(new String(packet.getData()));
+			buf = new byte[512];
+			if (r[0].equals("LOOKUP") && r.length == 2) {
+				System.out.println("R[1] " + r[1] + ";" + plates.size());
+				if (plates.get(r[1]) == null)
+					answer = "ERROR";
+				else
+					answer = plates.get(r[1]);
+			} else if (r[0].equals("REGISTER") && r.length == 3) {
+				if (plates.get(r[1]) == null) {
+					plates.put(r[1], r[2]);
+					answer = "OK";
+				} else
+					answer = "ERROR";
+			}
+			DatagramPacket sent = new DatagramPacket(answer.getBytes(),
+					answer.getBytes().length, recieved.getSocketAddress());
+			socket.send(sent);
 		}
 	}
 }
